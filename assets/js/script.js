7ceacff34295a09fff310c454361c8e6a8912322
Alp Burak Catakoglu <alpburak@hotmail.com>
2021-02-21 12:22:24 +0300

    testimonial

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,35 @@ $(window).on('load', function () {
 (function ($) {
   'use strict';
 
-  
+/* ========================================================================= */
+		/*	Testimonial Carousel
+		/* =========================================================================  */
+
+		//Init the slider
+		$('.testimonial-slider').slick({
+			slidesToShow: 2,
+			slidesToScroll: 1,
+			infinite: true,
+			arrows: false,
+			autoplay: true,
+			autoplaySpeed: 2000,
+			responsive: [{
+					breakpoint: 600,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 2
+					}
+				},
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
+					}
+				}
+			]
+		});
+      
   // product Slider
   $('.product-image-slider').slick({
     autoplay: false,
